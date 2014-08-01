73683e9b8a7721a367ccd24dba8c54bbf405fdd8
Johannes Ernst <jernst@indiecomputing.com>
2014-08-01 19:59:26 +0000

    fixed dependency added test

---
+++ selfoss/tests/Selfoss1Test.pm
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+#
+# Simple test for selfoss
+#
+# Copyright (C) 2012-2014 Indie Computing Corp.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+use warnings;
+
+package Selfoss1Test;
+
+use IndieBox::WebAppTest;
+
+# The states and transitions for this test
+
+my $custPointValues = {
+        'adminlogin' => 'specialuser',
+        'adminpass'  => 'specialpass'
+};
+
+my $TEST = new IndieBox::WebAppTest(
+    name                     => 'Selfoss1AppTest',
+    appToTest                => 'selfoss',
+    hostname                 => 'selfoss-test',
+    testContext              => '/foobar',
+    customizationPointValues => $custPointValues,
+
+    checks => [
+            new IndieBox::WebAppTest::StateCheck(
+                    name  => 'virgin',
+                    check => sub {
+                        my $c = shift;
+
+                        my $response = $c->httpGetRelativeContext( '/' );
+                        unless( $response->{headers} =~ m!HTTP/1.1 200 OK! ) {
+                            $c->reportError( 'Not HTTP Status 200', $response->{headers} );
+                        }
+                        					
+                        return 1;
+                    }
+            )
+    ]
+);
+
+$TEST;
